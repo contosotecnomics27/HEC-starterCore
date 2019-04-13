37220b6dc6618fbc52c218746e9563a2d6521098
contosoTecnomics@hotmail.com <contosoTecnomics@hotmail.com>
2019-04-13 22:14:41 +0500

    Add project files.

---
+++ HEC-starterCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HEC_starterCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
